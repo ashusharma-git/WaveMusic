e36124760c559dde4db7604cb487cc8bd0fc07d5
ASHU <ashusharma.email@gmail.com>
2022-04-13 22:49:33 +0530

    Wave Music player made with reactjs

---
+++ src/components/LibrarySong.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { playAudio } from "./util";
+
+const LibrarySong = ({song, songs, setCurrentSong, id, audioRef, isPlaying, setSongs}) => {
+    const songSelectHandler = async()=>{
+        // const selectedSong = songs.filter((state)=>state.id===id);
+        // setCurrentSong(selectedSong[0]);
+        await setCurrentSong(song)
+        const newSongs = songs.map((song)=>{
+            if(song.id===id){
+                return{
+                    ...song,
+                    active: true,
+                }
+            }
+            else{
+                return{
+                    ...song,
+                    active: false,
+                }
+            }
+        });
+        setSongs(newSongs);
+        if(isPlaying) audioRef.current.play();
+    }
+    return(
+        <div onClick={songSelectHandler} className={`library-song ${song.active ? 'selected' : ""}`}>
+            <img src={song.cover} alt="cover" />
+            <div className="song-description">
+                <h3>{song.name}</h3>
+                <h4>{song.artist}</h4>
+            </div>
+        </div>
+    )
+}
+export default LibrarySong;
